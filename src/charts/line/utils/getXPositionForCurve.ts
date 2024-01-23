f01b5a9e85b92a0897a7a9fae23f32876440acdc
simonitfi <simo.suurla@simonit.fi>
2024-01-23 14:47:54 +0200

    Update getXPositionForCurve.ts

--- src/charts/line/utils/getXPositionForCurve.ts
+++ src/charts/line/utils/getXPositionForCurve.ts
@@ -4,6 +4,8 @@ export function getXPositionForCurve(path: Path, index: number) {
   'worklet';
   if (index === 0) {
     return path.move.x;
+  }else if (index > path.curves.length){
+    return path.curves[path.curves.length - 1].to.x;
   }
   return path.curves[index - 1].to.x;
 }
