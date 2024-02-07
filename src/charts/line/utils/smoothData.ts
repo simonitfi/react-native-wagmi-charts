a16eabc2e274154ce5ccd6e4fa6aac4aaafa9962
Simo Suurla <simo.suurla@simonit.fi>
2024-02-07 13:51:10 +0200

    fix

--- src/charts/line/utils/smoothData.ts
+++ src/charts/line/utils/smoothData.ts
@@ -12,7 +12,6 @@ export const smoothData_ = (data: TLineChartData) => {
 };
 
 export const findPathIndex = (data: LineChartPath | LineChartArea, buffer: LineChartPathBuffer | LineChartAreaBuffer | null) => {
-  return -1
   if (typeof data.fromData !== 'number' || typeof data.toData !== 'number' || buffer === null) return -1
   const meta = JSON.stringify(data.meta)
 
@@ -30,7 +29,6 @@ export const findPathIndex = (data: LineChartPath | LineChartArea, buffer: LineC
 };
 
 export const findPath = (data: LineChartPath | LineChartArea, buffer: LineChartPathBuffer | LineChartAreaBuffer | null) => {
-  return
   if (typeof data.fromData !== 'number' || typeof data.toData !== 'number' || buffer === null) return
   const meta = JSON.stringify(data.meta)
 
