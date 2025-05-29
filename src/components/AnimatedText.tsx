4b138c9efa638df70754f96ef5b4152b9b9ce317
simonitfi <simo.suurla@simonit.fi>
2025-05-29 09:38:15 +0300

    Update AnimatedText.tsx

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -51,6 +51,7 @@ export const AnimatedText = ({ text, style }: AnimatedTextProps) => {
       underlineColorAndroid="transparent"
       editable={false}
       ref={Platform.select({ web: inputRef })}
+      value={text.value}
       style={[styles.text, style]}
       animatedProps={animatedProps}
     />
