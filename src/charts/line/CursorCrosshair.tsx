8937bc0c3356da56f6f8f5b9921abe78fd7b2018
Simo <simo.suurla@simonit.fi>
2026-03-01 16:45:14 +0200

    chore: remove fps debug counters (all idle at 0)

--- src/charts/line/CursorCrosshair.tsx
+++ src/charts/line/CursorCrosshair.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Platform, View, ViewProps, StyleSheet } from 'react-native';
 import Animated, {
   AnimatedProps,
+  runOnJS,
   useAnimatedReaction,
   useAnimatedStyle,
   useSharedValue,
